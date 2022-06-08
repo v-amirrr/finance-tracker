10f8338fea841f9c05dc6ae11b91ffb19732b354
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-06-08 15:17:48 +0430

    style: added a background, animations and some text in the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { motion } from "framer-motion"
+
+import styles from "./Homepage.module.css";
+
+const homepageTextVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: [10, -10, 0], transition: { delay: 1, duration: 0.4, type: "tween" } },
+}
+
+const homepageFooterVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0, transition: { delay: 1 } },
+}
+
 const Homepage = () => {
     return (
         <>
-            <div>
-                <Link to="/login">Start For Free</Link>
+            <div className={styles["home-page"]}>
+                <motion.div className={styles["homepage-text"]} initial="hidden" animate="visible" variants={homepageTextVariants}>
+                    <h1>Fianace Tracker</h1>
+                    <p>
+                        (Just In Case, If You Ever Need A Goddamn Finance Tracker)
+                        <br />
+                        (And Trust Me, You Do Not)
+                    </p>
+                    <motion.div className={styles["homepage-start-btn"]} whileTap={{ scale: 0.9 }}>
+                        <Link to="/login">START</Link>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div className={styles["footer"]} whileHover={{ scale: 1.5, y: -10 }}  initial="hidden" animate="visible" variants={homepageFooterVariants}>
+                    <p>Made With ❤ By <a href='https://github.com/v-amirrr' target="_blank" rel='noopener noreferrer'>ME</a>.</p>
+                </motion.div>
             </div>
         </>
     );
