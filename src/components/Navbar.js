31cf9603ae17e492ec251787b6f31d61a807a236
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-06-15 18:27:45 +0430

    style: added the animations to the navbar in AfterLogin page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,41 @@ import styles from "./Navbar.module.css";
 
 import { useLogout } from "../hooks/useLogout";
 
+import { motion } from 'framer-motion';
+
+const navbarVariants = {
+    hidden: { opacity: 0, y: -10 },
+    visible: { opacity: 1, y: [-10, 10, 0], transition: { delay: 0.4, duration: 0.4, type: "tween", when: "beforeChildren" } },
+    exit: {opacity: 0, y: -10, transition: { duration: 0.4, type: "tween" }}
+}
+
+const navbarTitleVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { delay: 0.4, duration: 0.4, type: "tween" } },
+    exit: {opacity: 0, x: -100, transition: { duration: 0.4, type: "tween" }}
+}
+
+const navbarBtnVariants = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0, transition: { delay: 0.4, duration: 0.4, type: "tween" } },
+    exit: {opacity: 0, x: 100, transition: { duration: 0.4, type: "tween" }}
+}
+
 const Navbar = () => {
+
     const { logout } = useLogout();
 
     return (
         <>
-            <div className={styles["navbar"]}>
-                <div>
+            <motion.div className={styles["navbar"]} initial="hidden" animate="visible" exit="exit" variants={navbarVariants}>
+                <motion.div variants={navbarTitleVariants}>
                     <h1>Fianace Tracker</h1>
-                </div>
+                </motion.div>
 
-                <div>
-                    <button onClick={logout}>Logout</button>
-                </div>
-            </div>
+                <motion.div variants={navbarBtnVariants}>
+                    <motion.button onClick={logout} whileTap={{ scale: 0.9 }}>Logout</motion.button>
+                </motion.div>
+            </motion.div>
         </>
     );
 };
